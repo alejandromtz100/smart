6c6f2cea59a20eeeca499f46a9249c6496b6955d
Alejandro Martinez <alejandro.martinez.22s@utzmg.edu.mx>
2025-07-08 17:59:47 -0600

    aver si funciona 2.5

--- movimientocontroller.js
+++ movimientocontroller.js
@@ -2,12 +2,16 @@ const Movimiento = require('./models/movimiento');
 
 exports.guardarMovimiento = async (req, res) => {
     try {
-        const { movimiento, x, y, z, ritmo } = req.body; // 💓 Agregado `ritmo`
-        const nuevo = new Movimiento({ movimiento, x, y, z, ritmo }); // 💓 También aquí
+        console.log("📩 Datos recibidos:", req.body); // 👈
+
+        const { movimiento, x, y, z, ritmo } = req.body;
+        const nuevo = new Movimiento({ movimiento, x, y, z, ritmo });
         await nuevo.save();
+
         res.status(201).json({ message: 'Movimiento guardado correctamente' });
     } catch (err) {
         console.error('Error al guardar movimiento:', err);
         res.status(500).json({ error: 'Error interno del servidor' });
     }
 };
+
